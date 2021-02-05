c70ad4de11e1081be76e0afaf47b80ef30778086
Edith Garrido <edith.garrido001@gmail.com>
2021-02-04 21:22:53 -0600

    link de tags funcnionando

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const descargarPropiedades = new Promise((resolve, reject) => {
+const descargarBlog = new Promise((resolve, reject) => {
   //const api = `datos/arreglo.json`;
   const api = `datos/datos.json`;
   console.log(api);
@@ -29,9 +29,12 @@ const descargarPropiedades = new Promise((resolve, reject) => {
 
 }); */
 
-function listarPropiedades(articulos) {
-  console.log('articulos: ' + articulos);
+function listarBlog(articulos) {
+  console.log('articulos h: ' + articulos);
   let html = '';
+  let articulo_home = '';
+
+  
   articulos.forEach(articulo => {
     console.log(articulo);
 
@@ -40,8 +43,7 @@ function listarPropiedades(articulos) {
           <img src="images/${articulo.articulo.imagen}" class=" card-img-top" alt="...">
           <div class="card-body" >
             <img src="images/${articulo.articulo.autor.imagen_autor}" class="rounded-circle img-thumbnail top-content center" alt="..." style="max-width: 80px; height: auto;">
-            <h5 class="card-title">| ${articulo.articulo.titulo}
-            <button type="button"  id="${articulo.articulo.id}" class="card-title" onclick="verPropiedad(this)">${articulo.articulo.titulo}</button></h5>
+            <h5 class="card-title"><button type="button"  id="${articulo.articulo.id}" class="card-title boton-titulo" onclick="verArticulo(this)">| ${articulo.articulo.titulo}</button></h5>
             <p class="card-text text-end .fs-6 texto">${articulo.articulo.autor.nombre_autor}</p>
           </div>
           <div class="card-footer">
@@ -61,9 +63,24 @@ function listarPropiedades(articulos) {
     
     `;
 
+
+    //articulo_home+= `<img src="images/${articulos.b.imagen}" class="card-img-top" alt="...">`;
+
+
   });
+
+  
+  //articulo_home+= `<img src="images/${articulos.articulo.imagen}" class="card-img-top" alt="...">`;
+
+
+
+
   const contenedorApp = document.querySelector('#card_article');
   contenedorApp.innerHTML = html;
+
+  /* const contenedorArticuloH = document.querySelector('#articulo_home');
+  contenedorArticuloH.innerHTML = articulo_home; */
+
   /* const y = document.getElementById("detalle2");
   y.style.display = "none";
   const z = document.getElementById("encabezadoDetalle");
@@ -71,10 +88,10 @@ function listarPropiedades(articulos) {
 }
 
 
-function verPropiedad(comp) {
+function verArticulo(comp) {
   let id = comp.id;
   console.log(id);
-  myObj.ver_propiedad_detalle(id);
+  myObj.ver_articulo_detalle(id);
 }
 
 function verDetalle(articulos, id) {
@@ -83,13 +100,38 @@ function verDetalle(articulos, id) {
   let tituloArticulo = '';
   let imagenArticulo = '';
   let textoArticulo = '';
+  let tarjetaAutor = '';
+  let tagsArticulo = '';
 
   const detail = articulos.find(articulo => {
     return articulo.articulo.id === id;
   });
-  tituloArticulo = `<h1>| ${detail.articulo.titulo}</h1>`;
+    tituloArticulo = `<h1>| ${detail.articulo.titulo}</h1>`;
     imagenArticulo = `<img src="images/${detail.articulo.imagen}" class="card-img-top autor-a" alt="...">`;
     textoArticulo = `<p class="text-wrap articulo">${detail.articulo.texto_articulo} </p>`;
+    tarjetaAutor = `<div class="row g-0">
+    <div class="col-md-4" >
+      <img src="images/${detail.articulo.autor.imagen_autor}" class="rounded-circle img-thumbnail" alt="..." >
+    </div>
+    <div class="col-md-8">
+      <div class="card-body mx-auto" >
+        <h4 class="card-title mb-0">${detail.articulo.autor.nombre_autor}</h4>
+        <p class="card-text mb-0">${detail.articulo.autor.titulo_autor}</p>
+        <p class="card-text mb-0"><small class="text-muted">${detail.articulo.autor.empresa_autor}</small></p>
+      </div>
+    </div>
+  </div>`;
+ let tagsarrayaDividir = detail.articulo.tags_articulo;
+  var arrayDeCadenas = tagsarrayaDividir.split(",");
+
+  for (var i=0; i < arrayDeCadenas.length; i++) {
+    tagsArticulo = `
+    <button type="button" class="btn btn-outline-secondary d-lg-inline-block mb-2" style="width: 100%;">${arrayDeCadenas[i]}</button>   
+    `;
+    //document.write(arrayDeCadenas[i] + " / ");
+ }
+
+ tagsArticulo +=  tagsArticulo ;
 
   const contenedorTitulo = document.getElementById("titulo_articulo");
   contenedorTitulo.innerHTML = tituloArticulo;
@@ -99,7 +141,13 @@ function verDetalle(articulos, id) {
 
   const contenedorApp2 = document.querySelector('#texto_articulo');
   contenedorApp2.innerHTML = textoArticulo;
+  
+  const contenedorTarjetaAutor = document.querySelector('#tarjeta_autor');
+  contenedorTarjetaAutor.innerHTML = tarjetaAutor;
 
+  const contenedorTagsArticulo = document.querySelector('#tags_articulo');
+  contenedorTagsArticulo.innerHTML = tagsArticulo;
+  
  
 
  /*  const y = document.getElementById("detalle2");
@@ -115,17 +163,18 @@ function verDetalle(articulos, id) {
 var myObj = {
   init: function() {
 
-    descargarPropiedades
+    descargarBlog
       .then(
-        listar => listarPropiedades(listar),
+        listar => listarBlog(listar),
         error => console.error(
           new Error('Hubo un error' + error)
         )
       );
-
+      
+     
   },
-  ver_propiedad_detalle: function(id) {
-    descargarPropiedades
+  ver_articulo_detalle: function(id) {
+    descargarBlog
       .then(
         detalle => verDetalle(detalle, id),
         error => console.error(
